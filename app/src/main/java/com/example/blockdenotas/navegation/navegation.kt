bf43204c3aaab639c64405084386841952651c83
Lautaro <lautarot2009@gamil.com>
2025-01-07 18:33:14 -0300

    2.6.2

--- app/src/main/java/com/example/blockdenotas/navegation/navegation.kt
+++ app/src/main/java/com/example/blockdenotas/navegation/navegation.kt
@@ -1,6 +1,5 @@
 package com.example.blockdenotas.navegation
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -10,23 +9,32 @@ import androidx.navigation.navArgument
 import com.example.blockdenotas.screens.MainNote
 import com.example.blockdenotas.screens.MainPage
 
+sealed class Screen(val route: String) {
+    object Main : Screen("main")
+    object Note : Screen("note")
+}
+
 @Composable
-fun AppNavegation(){
+fun AppNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "main_page") {
-        composable("main_page") {
+    NavHost(navController = navController, startDestination = Screen.Main.route) {
+        composable(Screen.Main.route) {
             MainPage(navController)
         }
         composable(
-            route = "note_page/{noteId}",
-            arguments = listOf(navArgument("noteId") { type = NavType.IntType })
+            route = "note/{noteId}",
+            arguments = listOf(
+                navArgument("noteId") {
+                    type = NavType.IntType
+                    defaultValue = -1 // Usamos -1 como indicador de "nueva nota"
+                }
+            )
         ) { backStackEntry ->
             val noteId = backStackEntry.arguments?.getInt("noteId")
-            // Your note details composable, passing noteId
-            if (noteId != null) {
-                MainNote(navController, noteId)
-            }
+            MainNote(
+                navController = navController,
+                noteId = if (noteId == -1) null else noteId // Convierte -1 a null para notas nuevas
+            )
         }
     }
-
 }
